1974c8ab54d2ef592d71e0965f2f1514f0d59c09
GazalChauhan <rajputgajl@gmail.com>
2024-07-29 18:08:58 +0530

    Add files via upload

--- password generator.py
+++ password generator.py
@@ -3,18 +3,18 @@ import string
 import random
  
  
-# store all characters in lists 
+# storing  characters in lists 
 s1 = list(string.ascii_lowercase)
 s2 = list(string.ascii_uppercase)
 s3 = list(string.digits)
 s4 = list(string.punctuation)
  
  
-# Ask user about the number of characters
+
 user_input = input("How many characters do you want in your password? ")
  
  
-# check this input is it number? is it more than 8?
+#  is it more than 8?
 while True:
  
     try:
@@ -36,7 +36,11 @@ while True:
  
         user_input = input("How many characters do you want in your password? ")
  
- 
+
+# calculate 30% & 20% of number of characters
+part1 = round(characters_number * (30/100))
+part2 = round(characters_number * (20/100))
+  
 # shuffle all lists
 random.shuffle(s1)
 random.shuffle(s2)
@@ -44,28 +48,30 @@ random.shuffle(s3)
 random.shuffle(s4)
  
  
-# calculate 30% & 20% of number of characters
-part1 = round(characters_number * (30/100))
-part2 = round(characters_number * (20/100))
+
  
  
-# generation of the password (60% letters and 40% digits & punctuations)
+# generation of the password 
 result = []
- 
+   
+
 for x in range(part1):
     result.append(s1[x])
     result.append(s2[x])
  
+
+
 for x in range(part2):
  
     result.append(s3[x])
     result.append(s4[x])
  
+
  
-# shuffle result
+# shuffle 
 random.shuffle(result)
  
  
-# join result
+# join 
 password = "".join(result)
-print("Strong Password: ", password)
+print("your strong Password: ", password)
